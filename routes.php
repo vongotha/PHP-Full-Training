4e53d29117b340aba647f85e87f32c6430aa9865
Argon Dev <vongotha0@gmail.com>
2025-08-07 01:07:29 +0100

    Update Routes -- demo/

--- routes.php
+++ routes.php
@@ -3,7 +3,7 @@
 
     return [
         '/' => 'controllers/index.php',
-        '/demo/' => 'controllers/index.php',
+        '/demo' => 'controllers/index.php',
         '/demo/about' => 'controllers/about.php',
         '/demo/notes' => 'controllers/notes/index.php',
         '/demo/note' => 'controllers/notes/show.php',
