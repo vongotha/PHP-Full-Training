be6eb1ea0bcacafd77fa24c0371b173a13c9d662
Argon Dev <vongotha0@gmail.com>
2025-08-08 02:13:36 +0100

    Project Organization -- PHP Autoloading - Fixing Heading from Banner.php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 
 const BASE_PATH = __DIR__ . '/../';
-var_dump(BASE_PATH);
+
 
 require BASE_PATH . 'functions.php';
 
--- views/notes/index.view.php
+++ views/notes/index.view.php
@@ -2,7 +2,7 @@
 
 <?php views("partials/nav.php"); ?>
 
-<?php views("partials/banner.php"); ?>
+<?php views("partials/banner.php", ['heading' => 'Notes']); ?>
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <?php foreach($notes as $note): ?>
