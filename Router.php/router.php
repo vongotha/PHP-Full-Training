2f1333e088f960c165d27f6c017dfb5562300253
Argon Dev <vongotha0@gmail.com>
2025-08-09 05:47:33 +0100

    Prpject Organization -- Build a better Router - Renaming Core To Router.php

--- Router.php/router.php
+++ Router.php/router.php
@@ -1,4 +1,7 @@
 <?php
+
+
+/*
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
@@ -21,4 +24,4 @@
         die();
     }
     
-    routeToController($uri, $routes);
+    routeToController($uri, $routes); 
