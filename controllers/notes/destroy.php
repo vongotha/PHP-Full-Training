7a8220f4ec1b461431d6545262f0de8207a1cff5
Argon Dev <vongotha0@gmail.com>
2025-08-15 00:56:43 +0100

    Project Organization -- Build a better Router - Setup new Routes: Create destroy.php

---
+++ controllers/notes/destroy.php
@@ -0,0 +1,27 @@
+<?php
+
+use Core\Database;
+// connect to our MySQL database
+$config = require base_path('config.php');
+$db = new Database($config['database'], 'root', '');
+
+$currentUserId = 3;
+
+$note = $db->query("select * from notes where  id = :id;", 
+['id' => $_POST['id']])->findOrFail();
+
+
+
+    $note = $db->query("select * from notes where  id = :id;", 
+['id' => $_POST['id']])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    $db->query("delete from notes where id = :id", [
+        'id' => $DELETE['id']
+    ]);
+
+
+    header('location: /demo/notes');
+    exit();
+
