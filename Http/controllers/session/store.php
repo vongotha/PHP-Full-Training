7aefc93f8875c11553e4a3c035f8788e3fbc7942
Argon Dev <vongotha0@gmail.com>
2025-09-07 02:45:39 +0100

    Project Organization -- Automatically Redirect Back

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -9,15 +9,13 @@ $password = $_POST['password'];
 
 $form = new LoginForm();
 
-if ($form->validate($email, $password)) {
-    $auth = new Authenticator();
+$form->validate($email, $password);
 
-    if ($auth->attempt($email, $password)) {
+    if ((new Authenticator())->attempt($email, $password)) {
         redirect('/demo');
     } else {
         $form->error('email', 'No user found with that email and password combination.');
     }
-}
 
 Session::flash('errors', $form->errors());
 
