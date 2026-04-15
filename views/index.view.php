94a58bc9a7074c618247e4ec143b956d88c53291
Argon <vongotha0@gmail.com>
2026-04-16 00:24:17 +0100

    Mes modifications locales avant rebase

--- views/index.view.php
+++ views/index.view.php
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-<?php require "views/partials/head.php"; ?>
-    
-<?php require "views/partials/nav.php"; ?>
-
-<?php require "views/partials/banner.php"; ?>
-
-<main>
-    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    <p>Hey, you're in the Home page</p>
-    </div>
-</main>
-<?php require "views/partials/footer.php"; ?>
-=======
 <?php views("partials/head.php"); ?>
     
 <?php views("partials/nav.php"); ?>
@@ -24,4 +10,3 @@
     </div>
 </main>
 <?php views("partials/footer.php"); ?>
->>>>>>> 4aec7e0 (Dockerisation)
